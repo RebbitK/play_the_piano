9ca10b3beb18038e0d0a6b32cd7580c9c3103b90
RebbitK <andxl9979@gmail.com>
2024-12-03 23:47:19 +0900

    :sparkles: 프로젝트 시작

---
+++ src/main/java/com/example/play_the_piano/PlayThePianoApplication.java
@@ -0,0 +1,13 @@
+package com.example.play_the_piano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayThePianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayThePianoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/play_the_piano/PlayThePianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.play_the_piano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayThePianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
