0963323047cb52a22bc8b962b940c1be45c05ea5
RebbitK <andxl9979@gmail.com>
2024-12-09 22:03:42 +0900

    :recycle: RefreshTokenMapper 수정

--- src/main/java/com/example/play_the_piano/refreshToken/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/play_the_piano/refreshToken/repository/RefreshTokenRepository.java
@@ -1,12 +1,11 @@
 package com.example.play_the_piano.refreshToken.repository;
 
 import com.example.play_the_piano.refreshToken.entity.RefreshToken;
-import com.example.play_the_piano.user.entity.User;
 import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface RefreshTokenRepository  {
+public interface RefreshTokenRepository {
 
 	Optional<RefreshToken> findByUserId(Long userId);
 
