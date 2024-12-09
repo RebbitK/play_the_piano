e95045b379b78c7635887c929c663a64bd750e0d
RebbitK <andxl9979@gmail.com>
2024-12-09 21:59:02 +0900

    :sparkles: 보안용 Config 추가

---
+++ src/main/java/com/example/play_the_piano/global/config/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.example.play_the_piano.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final long MAX_AGE_SECS = 3600;
+
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		String[] allowedOrigins = {"http://localhost:3000","http://localhost:3001","http://localhost:8080"};
+
+		registry.addMapping("/**")
+			.exposedHeaders("Authorization")
+			.allowedOriginPatterns(allowedOrigins)
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(MAX_AGE_SECS);
+	}
+}
---
+++ src/main/java/com/example/play_the_piano/global/config/WebSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.example.play_the_piano.global.config;
+
+import com.example.play_the_piano.global.security.JwtAuthorizationFilter;
+import com.example.play_the_piano.global.util.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+	private final JwtUtil jwtUtil;
+	private final ObjectMapper objectMapper;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+		throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public JwtAuthorizationFilter jwtAuthorizationFilter() {
+		return new JwtAuthorizationFilter(jwtUtil, objectMapper);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.csrf((csrf) -> csrf.disable());
+
+		http.sessionManagement((sessionManagement) ->
+			sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		);
+
+		http.authorizeHttpRequests((authorizeHttpRequests) ->
+
+			authorizeHttpRequests
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+				.permitAll()
+				.requestMatchers("/auth/**").permitAll()
+				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+				.requestMatchers(HttpMethod.GET,"/health-check").permitAll()
+				.anyRequest().authenticated()
+
+		);
+
+		// 필터 관리
+		http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+}
