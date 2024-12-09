085a6117d374057285f8f3fb5703730e6b6469fb
RebbitK <andxl9979@gmail.com>
2024-12-09 21:56:18 +0900

    :sparkles: MapperScan 추가

--- src/main/java/com/example/play_the_piano/PlayThePianoApplication.java
+++ src/main/java/com/example/play_the_piano/PlayThePianoApplication.java
@@ -1,9 +1,12 @@
 package com.example.play_the_piano;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@MapperScan("com.example.play_the_piano")
 public class PlayThePianoApplication {
 
 	public static void main(String[] args) {
